6b0cfd8f7dc9a194259ea89ed2356be9cf38ee9b
Xiangpeng Hao <xha62@sfu.ca>
2020-05-11 16:48:35 -0700

    now works with python

--- README.md
+++ README.md
@@ -11,3 +11,21 @@ We sometimes need to process hundreds of such text files, thus need a programmat
 1. A shared library that can be called from Python
 2. A Webassembly version to support pibench-online.
 3. Native rust version for pibench backend server.
+
+
+## Work with Python
+
+```python
+import ctypes
+from ctypes import c_char_p, c_void_p, CDLL
+
+so_file = "/path/to/libpibench_parser.so"
+functions = CDLL(so_file)
+
+def text_to_json(text):
+    json_str = functions.text_to_json(text.encode("utf-8"))
+    try:
+        return ctypes.cast(json_str, c_char_p).value.decode("utf-8")
+    finally:
+        functions.free_json_str(json_str)
+```
--- src/lib.rs
+++ src/lib.rs
@@ -183,6 +183,29 @@ impl PiBenchData {
     }
 }
 
+use libc::c_char;
+use std::ffi::{CStr, CString};
+#[no_mangle]
+pub extern "C" fn text_to_json(text: *const c_char) -> *mut c_char {
+    let c_str = unsafe {
+        assert!(!text.is_null());
+        CStr::from_ptr(text).to_str().unwrap()
+    };
+    let rv = PiBenchData::from_text(c_str).unwrap().to_json();
+    let c_str_rv = CString::new(rv).unwrap();
+    c_str_rv.into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn free_json_str(val: *mut c_char) {
+    unsafe {
+        if val.is_null() {
+            return;
+        }
+        CString::from_raw(val);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
